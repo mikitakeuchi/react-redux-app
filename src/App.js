9bd4df8ead60d3cf8146608636eabe0db08da53c
mikitakeuchi <miki.mori@mac.com>
2020-02-11 04:55:46 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React, { Component } from "react";
+// import React, { Component } from "react"; // class component
+import React from "react"; // 関数component
 
-class App extends Component {
-  render() {
-    const greeting = 'Hi!';
-    return (
-      <React.Fragment>
-        <h1 className='foo'>{greeting}</h1>
-        <label htmlFor='bar'>bar</label>
-        <input type='text' onClick={() => {console.log('I am clicked!')}} />
-        <input type='text' onChange={() => {console.log('I am changed!')}} />
-      </React.Fragment>
-    );
-  }
+// class component ...
+// class App extends Component {
+//   render() {
+//     const greeting = 'Hi!';
+//     return (
+//       <React.Fragment>
+//         <h1 className='foo'>{greeting}</h1>
+//         <label htmlFor='bar'>bar</label>
+//         <input type='text' onClick={() => {console.log('I am clicked!')}} />
+//         <input type='text' onChange={() => {console.log('I am changed!')}} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// 関数component
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
