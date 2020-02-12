533f8bb273c60ff4277995c9770ee39765e42e85
mikitakeuchi <miki.mori@mac.com>
2020-02-13 01:03:12 +0900

    implement events index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,4 @@
 import { combineReducers} from 'redux'
-import count from './count'
+import events from './events'
 
-export default combineReducers({ count })
-
-
-// 複数ある場合以下のように記述できる
-// export default conbineReducers({ foo, bar, baz })
+export default combineReducers({ events })
